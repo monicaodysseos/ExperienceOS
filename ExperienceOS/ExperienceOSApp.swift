bf877a57cb5132c5a62ff70c5574c6493dbfd199
PantelisPolycarpou <pantelispolyc@gmail.com>
2026-02-28 11:08:32 +0200

    Initial Commit

---
+++ ExperienceOS/ExperienceOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperienceOSApp.swift
+//  ExperienceOS
+//
+//  Created by Monica Odysseos on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExperienceOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
